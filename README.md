0da5331245fb3acd921af3e001f36a8d855c0f68
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2019-01-30 02:24:51 +0200

    modified:   README.md 	modified:   mazelib/try.py

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
 # RCJT
-Code for RCJ robot
-* OCR
-* IR 
-* Heated Victim Detection
-* Movement Strategy
-* Strategy Flowchart
-* Skeleton Code
-* ~~Git Repo~~
-* Movement Code
-* Rescue Kit Delivery System
-* Chaise Design
-* ~~Motor Sizing~~
-* Battery Calculation
-* Trap RGB system
--- mazelib/try.py
+++ mazelib/try.py
@@ -1,17 +1,12 @@
 from mazelib import *
 
-
-
-
-
 m = Maze()
 m.generator = Prims(5, 5)
 m.generate()
 
-
-m.solver =rcj()
+m.solver =rcj_rat_mohamed()
 m.generate_entrances()
-m.solve()
 print(m.tostring(True))        # print walls and entrances
+m.solve()
 print(m.tostring(True, True))  # print walls, entrances, and solution
 
