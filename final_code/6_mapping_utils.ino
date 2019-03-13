d0a183114f2913126a114a892f64f485e0e86dbc
mohamed emad <mohamedemad4bubble@gmail.com>
2019-03-13 14:24:32 +0400

    New tools

--- final_code/6_mapping_utils.ino
+++ final_code/6_mapping_utils.ino
@@ -61,3 +61,67 @@ void PrintMatrix()
    }
    Serial.println();
 }
+
+void update_matrix(){
+    //call update_matrix();
+    int lt=count_tiles(left_us);
+    int ct=count_tiles(center_us);
+    int rt=count_tiles(right_us);
+    DEBUG_RAW("update_matrix() current orintation is: ");
+    DEBUG_INT(orintation);
+    if (orintation==0){
+        append_to_matrix(lt,3);
+        append_to_matrix(ct,0);
+        append_to_matrix(rt,1)
+    }
+    if(orintation==1){
+        append_to_matrix(lt,0);
+        append_to_matrix(ct,1);
+        append_to_matrix(rt,2);
+    }
+    if (orintation==2){
+        append_to_matrix(lt,1);
+        append_to_matrix(ct,2);
+        append_to_matrix(rt,3);
+    }
+    if (orintation==3){
+        append_to_matrix(lt,2);
+        append_to_matrix(ct,3);
+        append_to_matrix(rt,0);
+    }
+} 
+
+void append_to_matrix(int Ntiles,int ao){ 
+    /*
+    ao represents the matrix's internal orintation variable
+    since you will want to append to certien directions relative to the start tile 
+    it follows the same logic ,0=F,1=R,2=B,3=L 
+    */
+    DEBUG_RAW("append_to_matrix: ");
+    DEBUG_INT(Ntiles);
+    DEBUG_INT(ao);
+    if(Ntiles==0){
+        return;
+    }
+    if(ao==0){
+        for (int i=posX;i<=Ntiles+posX;i++){ 
+            grid_matrix[i][posY]=1;
+        }
+    }
+    if(ao==2){
+        for (int i=posX;i>=posX-Ntiles;i--){ 
+            grid_matrix[i][posY]=1;
+        }            
+    }   
+    
+    if(ao==1){
+        for (int i=posY;i<=Ntiles+posY;i++){ 
+            grid_matrix[posX][i]=1;
+        }
+    }
+    if(ao==3){
+        for (int i=posY;i>=posY-Ntiles;i--){ 
+            grid_matrix[posX][i]=1;
+        }            
+    }
+}   
