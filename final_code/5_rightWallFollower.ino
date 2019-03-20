4ea7e21ca230ff0cd87f72ca73fdaaeeeb3d8faa
mohamed emad <mohamedemad4bubble@gmail.com>
2019-03-20 11:09:41 +0400

    'fixed' logic

--- final_code/5_rightWallFollower.ino
+++ final_code/5_rightWallFollower.ino
@@ -1,26 +1,54 @@
 void rightWallfollower(){
   //right wall follower 
-      while(GetDist(center_us)>15){
+      while(GetDist(center_us)>25){
         drive_forward();
         }
  
           //!!!dont forget to calibrate the us_  in the maze!!!
           
-         if(GetDist(center_us)<=15  ){  //found a wall infront of you!
+         if(GetDist(center_us)<=25  ){  //found a wall infront of you!
           
-          if(GetDist(right_us)>=7){//turn right if there isn't a wall on the right
-            turn(90,1);
-
+          if(GetDist(right_us)>=5){//turn right if there isn't a wall on the right
+            turn_right();
+            delay(600);
+            //turn(90,1);
           }
 
-            else if(GetDist(right_us)<7){// turn left if there is a wall on the right
-              turn(90,0);
+            else if(GetDist(right_us)<5){// turn left if there is a wall on the right
+              turn_left();
+              delay(600);
+              //turn(90,0);
               }
          }
 
-          if(GetDist(right_us)>=7){//always right
+          
+            align_and_emergency();
+            trap_cond();
+}
+void leftWallfollower(){
+  //right wall follower 
+      while(GetDist(center_us)>25){
+        drive_forward();
+        }
+ 
+          //!!!dont forget to calibrate the us_  in the maze!!!
+          
+         if(GetDist(center_us)<=25  ){  //found a wall infront of you!
+          
+          if(GetDist(left_us)>=5){//turn right if there isn't a wall on the right
+            //turn_right();
+            //delay(600);
+            turn(90,0);
+          }
+
+            else if(GetDist(right_us)<5){// turn left if there is a wall on the right
+              //turn_left();
+              //delay(600);
               turn(90,1);
-            }
+              }
+         }
+
+          
             align_and_emergency();
             trap_cond();
 }
